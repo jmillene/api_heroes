d0ef6d4f4e6b901c58f717f0a4c329aff2d2aeef
jessica <jmillene12@gmail.com>
2022-12-05 20:35:23 -0300

    renderizando heroes

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,6 +1,9 @@
 import * as express from 'express';
+import  * as cors from 'cors';
 import heroesRouter from './routes/index';
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use('/heroes', heroesRouter.heroesRouter )
 export default app;
